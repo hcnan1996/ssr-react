3600e68368fcba36b22ed9f89bf9e57721b44a81
hcnan <hcnan@histudy.com>
2020-12-20 20:55:44 +0800

    同构

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -2,7 +2,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div>This is home hcnan96</div>
+      <div>This is home hcnan1996</div>
       <button
         onClick={() => {
           alert("666");
