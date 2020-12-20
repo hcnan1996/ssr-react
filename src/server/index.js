3dcd650687eb16461a1568fe72221df844c23b8c
hcnan <hcnan@histudy.com>
2020-12-20 15:48:26 +0800

    简单ssr

---
+++ src/server/index.js
@@ -0,0 +1,25 @@
+// server/index.js
+import express from 'express';
+import React from 'react'
+import { renderToString } from 'react-dom/server';
+import Home from '../containers/home';
+
+const app = express();
+const content = renderToString(<Home />);
+app.get('/', function (req, res) {
+  res.send(
+  `
+  <html>
+    <head>
+      <title>ssr</title>
+    </head>
+    <body>
+      <div id="root">${content}</div>
+    </body>
+  </html>
+  `
+  );
+})
+app.listen(3001, () => {
+  console.log('listen:3001')
+})
---
+++ webpack.server.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+const merge = require('webpack-merge')
+const config = require('./webpack.base')
+
+const serverConfig = {
+  target: 'node',
+  mode: 'development',
+  entry: './src/server/index.js',
+  externals: [nodeExternals()],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  }
+}
+
+module.exports = merge(config, serverConfig)
