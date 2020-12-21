b40c77c2b7d5e4e91f0dd269cc52c1644a6723a1
hcnan <hcnan@histudy.com>
2020-12-21 10:01:06 +0800

    css init

--- webpack.server.js
+++ webpack.server.js
@@ -1,17 +1,33 @@
-const path = require('path')
-const nodeExternals = require('webpack-node-externals')
-const merge = require('webpack-merge')
-const config = require('./webpack.base')
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const merge = require("webpack-merge");
+const config = require("./webpack.base");
 
 const serverConfig = {
-  target: 'node',
-  mode: 'development',
-  entry: './src/server/index.js',
+  target: "node",
+  mode: "development",
+  entry: "./src/server/index.js",
   externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.css?$/,
+        use: [
+          "isomorphic-style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')
-  }
-}
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+};
 
-module.exports = merge(config, serverConfig)
+module.exports = merge(config, serverConfig);
