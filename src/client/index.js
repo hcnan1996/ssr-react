8e6d1a8566bcbcb0c71dcae7596c1ccae9edb0d2
hcnan <hcnan@histudy.com>
2021-04-01 16:11:45 +0800

    hydrate

--- src/client/index.js
+++ src/client/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Home from '../containers/home';
 
+// 在服务端渲染和客户端首次渲染完全一致的情况下，才能使用hydrate替换render
 ReactDom.hydrate(<Home />, document.getElementById('root'))
